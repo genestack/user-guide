4ddd360b68b825cbc2fe15be90d6cb71a3c381e7
Qian Gao <pamela.qg@gmail.com>
2021-01-25 11:59:50 +0000

    Amgen_RFP
    R shiny app for Amgen RFP

---
+++ R-Shiny-apps/dictionaries_api.R
@@ -0,0 +1,54 @@
+library(httr)
+library(RJSONIO)
+
+host <- "occam.genestack.com"
+token <- "bd7ebdc3ea0ac8be98ecad5a7570589513885a81"
+
+authenticate = function() {
+    sign_in = sprintf('https://%s/frontend/endpoint/application/invoke/genestack/signin', host)
+    auth = httr::POST(sign_in, body = list(
+        method = "authenticateByApiToken",
+        parameters = sprintf('["%s"]', token)
+    ))
+}
+
+GetTemplateValues <- function() {
+    authenticate()
+    terms = httr::POST(
+        sprintf('https://%s/frontend/endpoint/application/invoke/genestack/shell', host),
+        body = list(
+            method = "dictionaryAutocomplete",
+            parameters = '["GSF516042",""]'
+        )
+    )
+    labels = fromJSON(rawToChar(terms$content))$result
+    
+    return(labels)
+}
+
+# GetCellSubtypes = function(cellType) {
+#     authenticate()
+#     terms = httr::POST(
+#         sprintf('https://%s/frontend/endpoint/application/invoke/genestack/shell', host),
+#         body = list(
+#             method = "getDictionaryChildren",
+#             parameters = sprintf('["GSF501948","subclass_of","%s"]', cellType)
+#         )
+#     )
+#     
+#     return(fromJSON(rawToChar(terms$content))$result)
+# }
+
+# GetGeneSynonyms = function(genes) {
+#     authenticate()
+#     terms = httr::POST(
+#         sprintf('https://%s/frontend/endpoint/application/invoke/genestack/shell', host),
+#         body = list(
+#             method = "getGeneSynonyms",
+#             parameters = sprintf('["GSF534821", "%s"]', genes)
+#         )
+#     )
+#     
+#     result = fromJSON(rawToChar(terms$content))$result
+#     return(do.call(rbind, result))
+# }

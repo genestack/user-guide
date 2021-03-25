2b981a633c8e15555a61ce02d4396d11a006cb37
Michael Nedzelsky <michael.nedzelsky@genestack.com>
2021-03-25 12:09:44 +0000

    fix

--- shiny-server/reports/table-viewer/app.R
+++ shiny-server/reports/table-viewer/app.R
@@ -7,17 +7,19 @@ library(RCurl)
 odm_host <- "occam.genestack.com"
 token <- "c7eb6c07150e851141e19b8bf07a2ceaee9cfef2"
 study_acc <- "GSF1235133"
-res_key <- "assoc_result_link"
-res_cmd_key <- "assoc_command_line"
+name <- 'GWAS Analysis'
+res_key <- paste(name, 'File', sep=' / ')
+res_cmd_key <- paste(name, 'Command Line', sep=' / ')
+res_desc_key <- paste(name, 'Description', sep=' / ')
 
 ui <- fluidPage(
-  titlePanel("Example"),
+  titlePanel(name),
   mainPanel(
-    h6("Command line"),
-    textOutput("cmdLine")
-  ),
-  p(),
-  DT::dataTableOutput("table")
+    br(),
+    tableOutput('metainfo'),
+    br(),
+    DT::dataTableOutput("table")
+  )
 )
 
 server <- function(input, output, session) {
@@ -26,16 +28,22 @@ server <- function(input, output, session) {
     query$studyId
   })
 
-  output$cmdLine <- renderText( {
+  output$metainfo <- renderTable( {
     client <- studyCurator::ApiClient$new(host    = odm_host,
                                           version = "default-released",
                                           token   = token)
     api    <- StudySPoTApi$new(client)
     
     study   <- api$get_study(study_acc)$content
-    command_line = as.character(study[res_cmd_key])
-    command_line
-  })
+    metainfo_names <- names(study)
+    names_cond <- lapply(metainfo_names, function(x) startsWith(x, name))
+    gwas_subset = study[metainfo_names[unlist(names_cond)]]
+    new_names = lapply(names(gwas_subset), function(x) substring(x, nchar(paste(name, " / ", sep=""))+1, ))
+    names(gwas_subset) <- new_names
+    ls = list(a=names(gwas_subset), b=unlist(gwas_subset))
+    df <- as.data.frame(ls)
+    df
+  }, colnames=FALSE, rownames=FALSE, bordered=TRUE)
   output$table <- DT::renderDataTable({
     client <- studyCurator::ApiClient$new(host    = odm_host,
                                            version = "default-released",
